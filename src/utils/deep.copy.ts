7350e71b495396a1d74a4f06b582e815cd17377a
Sebastiaan Arendsen <sebastiaan@arendsen.dev>
2022-04-29 01:52:02 +0200

    Implemented copy game state function

--- src/utils/deep.copy.ts
+++ src/utils/deep.copy.ts
@@ -1,3 +1,8 @@
+import { GameState } from '@/state/game.state';
 import rfdc from 'rfdc';
 
-export const clone = rfdc();
+const clone = rfdc();
+
+export function copyGameState(gameState: GameState) {
+  return clone(gameState);
+}
